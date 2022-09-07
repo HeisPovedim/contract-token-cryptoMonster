af429e716ae7543ecc3483489914313f90acaee8
Larionov Mikhail <larionov404.mu@gmail.com>
2022-09-07 12:21:52 +0300

    Рабочий токен

--- main.sol
+++ main.sol
@@ -1,48 +1,95 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract CryptoMonster {
-    address owner;
-    string name;
-    string symbol;
-    uint256 totalCryptoMonster;
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 
-    // создание привязки адресов к их балансу
-    mapping (address => uint256) public balance;
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalCryptoMonster) public {
-        owner = msg.sender;
-        name = _name;
-        symbol = _symbol;
-        totalCryptoMonster = _totalCryptoMonster;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-        balance[owner] = totalCryptoMonster;
+contract SampleToken is IERC20 {
+    using SafeMath for uint256;
+
+    // COMMENT: Общие сведения по токену.
+    string public constant name = "CryptoMonster";
+    string public constant symbol = "CMON";
+    uint8 public constant decimals = 12;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+
+    uint256 totalSupply_;
+
+    constructor(uint256 total) {
+        totalSupply_ = total;
+        balances[msg.sender] = totalSupply_;
     }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    function totalSupply() view public returns(uint256) {
-        return totalCryptoMonster;
-    }   
-
-    function balanceOf(address _owner) view public returns(uint256) {
-        return balance[_owner];
+    // COMMENT_FUNC: Функция вернет количество всех токенов, выделенных этим контрактом, независимо от владельца.
+    function totalSupply() public override view returns (uint256) {
+        return totalSupply_;
     }
 
-    function transfer(address _to, uint256 _value) public returns(bool) {
-        require (balance[msg.sender] > _value);
+    // COMMENT_FUNC: Функция вернет текущий баланс токена учетной записи, идентифицированный по адресу его владельца.
+    function balanceOf(address tokenOwner) public override view returns (uint256) {
+        return balances[tokenOwner];
+    }
 
-        address _from = msg.sender;
-        owner = _to;
-        emit Transfer(_from, _to, _value);
-        balance[_from] = balance[_from] - _value;
-        balance[_to] = balance[_to] + _value;
+    // COMMENT_FUNC: Функция перевода используется для перемещения количества токенов numTokens с баланса владельца
+    // на баланс другого пользователя или получателя. Передающий владелец — msg.sender, 
+    // то есть тот, кто выполняет функцию.
+    function transfer(address receiver, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[receiver] = balances[receiver].add(numTokens);
+        emit Transfer(msg.sender, receiver, numTokens);
         return true;
-
-
-
-
     }
 
+    // COMMENT_FUNC: Функция позволяет владельцу, т. е. msg.sender, одобрить делегированную учетную запись
+    // для снятия токенов со своей учетной записи и передачи их на другие учетные записи.
+    function approve(address delegate, uint256 numTokens) public override returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
 
+    // COMMENT_FUNC: Функци возвращает текущее утвержденное количество токенов владельцем
+    // конкретному делегату, как установлено в функции утверждения.
+    function allowance(address owner, address delegate) public override view returns (uint) {
+        return allowed[owner][delegate];
+    }
 
+    // COMMENT_FUNC: Функция TransferFrom является аналогом функции утверждения. Это позволяет делегату,
+    // одобренному для снятия средств, переводить средства владельца на сторонний счет.
+    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+}
+
+library SafeMath {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+      assert(b <= a);
+      return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+      uint256 c = a + b;
+      assert(c >= a);
+      return c;
+    }
 }
